64e58a68dbbea2bb18f1d7d96c16581301df3a13
王钰 <wangyu9@bonc.com.cn>
2022-03-16 14:56:13 +0800

    提交dev分支

---
+++ src/main/java/com/example/gitmergedemofinalx/GitMergeDemoFinalXApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemofinalx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergeDemoFinalXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMergeDemoFinalXApplication.class, args);
+    }
+
+}
