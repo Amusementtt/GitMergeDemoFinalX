8ce5fc1f701d23606beba95552db577f731e9f0b
王钰 <wangyu9@bonc.com.cn>
2022-03-16 15:03:21 +0800

    dev1分支提交222

--- src/main/java/com/example/gitmergedemofinalx/GitMergeDemoFinalXApplication.java
+++ src/main/java/com/example/gitmergedemofinalx/GitMergeDemoFinalXApplication.java
@@ -9,6 +9,7 @@ public class GitMergeDemoFinalXApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitMergeDemoFinalXApplication.class, args);
         System.out.println("111");
+        System.out.println("222");
     }
 
     public void testOne(){
